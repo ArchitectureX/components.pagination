e8a51c746eb63122ac19b5ec0ef9daddb9c46456
FoggDev <foggdev@gmail.com>
2023-10-12 16:11:02 -0600

    fix: Fixing hover on nav link

--- src/styles.ts
+++ src/styles.ts
@@ -3,7 +3,8 @@ import cx from '@architecturex/utils.cx'
 export const styles = {
   paginationList: 'flex items-center justify-center mt-4',
   li: 'm-1',
-  pageNavLink: (color: string) => `px-4 py-2 ${color} text-white rounded-full hover:bg-blue-700`,
+  pageNavLink: (color: string) =>
+    `px-4 py-2 ${color} text-white rounded-full hover:bg-blue-700 hover:no-underline`,
   pageNavLinkPge: 'px-4 py-2 bg-gray-200 rounded-full hover:bg-gray-300 hover:no-underline',
   pageNextLink: 'px-4 py-2 bg-gray-200 rounded-full hover:bg-gray-300 hover:no-underline',
   pagePreviousLink: 'px-4 py-2 bg-gray-200 rounded-full hover:bg-gray-300 hover:no-underline',
